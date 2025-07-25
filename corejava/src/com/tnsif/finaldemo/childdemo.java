7edf191d0f92954440e68ea7037d8ad7769de51b
Kantharaj B M <Kantharaj B M@LAPTOP-KNARGVEF>
2025-07-25 11:46:27 +0530

    commit

---
+++ corejava/src/com/tnsif/finaldemo/childdemo.java
@@ -0,0 +1,8 @@
+package com.tnsif.finaldemo;
+
+public class childdemo extends finaldemo{
+	void show() {
+		System.out.println("java");
+	}
+
+}
---
+++ corejava/src/com/tnsif/finaldemo/finaldemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.finaldemo;
+
+public class finaldemo {
+final static int a=5;
+	
+	static void display() {
+		a=10;
+	}
+	
+  final	void show() { 
+		System.out.println("welcome");
+	}
+
+}
+
+
